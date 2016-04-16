d6c0d2835d981388237e915c1985b913b61cadb5
George Tzoganakis <gtzogana@uwaterloo.ca>
2016-04-16 17:08:53 -0400

    Debug pass

--- code/keras_ae.py
+++ code/keras_ae.py
@@ -9,6 +9,7 @@ from keras.layers.core import Dense, Activation, Dropout, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
 from keras.utils import np_utils
+import keras
 
 # Other
 import matplotlib.pyplot as plt
@@ -19,23 +20,33 @@ TRAINING_RATIO = 0.7
 EPOCHS = 50
 
 np.random.seed(1)
-random.seed(1)
 
-patches = pp.load_patches()
-labels = pp.grab_labels(patches)
-training, validation = pp.divide_sets(TRAINING_RATIO, patches)
+print "Loading data."
+patches, labels = pp.load_patches()
+labels_list = pp.grab_labels(labels)
+training, validation, t_labels, v_labels = pp.divide_sets(TRAINING_RATIO, patches, labels)
 
-def build_network():
+def build_baseline_network():
     model = Sequential() # Linear stack of layers
 
-    model.add(Dense(16, input_dim=np.prod(INPUT_DIMENSION)) # Input (*, INPUT_DIM), output (*, 16)
+    # output size = 1000
+    model.add(Dense(1000, input_dim=pp.RESIZE_PIXELS, activation='tanh'))
     model.add(Dropout(0.5))
 
     # Classif. layer
     model.add(Dense(len(labels)))
     model.add(Activation('softmax'))
 
-    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd)
+    model.compile(loss='categorical_crossentropy', optimizer='Adam')
+    early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=1, verbose=0)
+
+    print 'Starting baseline training.'
+    model.fit(training, t_labels, nb_epoch=EPOCHS, batch_size=10, show_accuracy=True, verbose=True, callbacks=[early_stopping])
+    print 'Done baseline training.'
 
     return model
+
+print "Building network."
+net = build_baseline_network()
+score = net.evaluate(validation, v_labels, show_accuracy=True, verbose=False)
+print 'Accuracy: ' + str(score)
